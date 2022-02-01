f1b669ffa82698e5ebab19bd2f023fe6faeec17c
ametel01 <alex-metelli@gmx.com>
2022-02-01 21:30:06 +0000

    got test working

--- tests/test_contract.py
+++ tests/test_contract.py
@@ -7,27 +7,20 @@ from starkware.starknet.testing.contract import StarknetContract
 from starkware.starknet.compiler.compile import (
     compile_starknet_files)
 
-CONTRACT_FILE = os.path.join(
-    os.path.dirname(__file__), "ERC20.cairo")
-
-
-@pytest.fixture(scope='module')
-async def get_starknet():
-    starknet = await Starknet.empty()
-    return starknet
+CONTRACT_FILE = "/home/ametel01/Source/Cairo/cairo_library/contracts/finance/token/ERC20.cairo"
 
 
 @pytest.mark.asyncio
-async def contract_factory():
+async def test_constructor():
     contract_definition = compile_starknet_files(
         [CONTRACT_FILE], debug_info=True)
     starknet = await Starknet.empty()
     contract = await starknet.deploy(source=CONTRACT_FILE,
                                      constructor_calldata=[1111, 2222, 10000, 00, 12345])
-    # contract = StarknetContract(
-    #     starknet=starknet,
-    #     abi=contract_definition.abi,
-    #     contract_address=contract_address)
 
-    info = await contract.symbol().call()
-    assert info == 2222
+    name = await contract.name().call()
+    assert name.result.name == 1111
+    symbol = await contract.symbol().call()
+    assert symbol.result.symbol == 2222
+
+ # print(CONTRACT_FILE)
